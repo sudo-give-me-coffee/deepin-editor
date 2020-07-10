a1e986af1dff7004d17a233bf23f22c6fc2cd41e
Natanael <natanaelgplay@gmail.com>
2020-07-09 21:44:13 -0300

    Update build_appimage.sh

--- build_appimage.sh
+++ build_appimage.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo sed -i "$ deb http://deepin.c3sl.ufpr.br/deepin/ unstable contrib main non-free" "/etc/apt/sources.list"
+
+sudo apt update
+
 sudo apt install cmake libqt5widgets5 libdtkcore-dev libdtkwidget-dev qt5-default libpolkit-qt5-1-dev libkf5syntaxhighlighting-dev libkf5codecs-dev qttools5-dev-tools qtbase5-private-dev libxcb-util0-dev libdtkwm-dev libxtst-dev -y
 
 mkdir build
